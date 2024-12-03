8d6f35972139557cbe4acfb24c5ca2fa755ed951
UgUsDead <hugomorais412010@gmail.com>
2024-12-03 17:10:47 +0000

    form contacto e css

--- src/components/Contact.js
+++ src/components/Contact.js
@@ -6,7 +6,20 @@ const Contact = () => {
     <div className="contact">
       <h1>Contacto</h1>
       <p>Entre ai em contacto mano bro</p>
+
+      <form>
+      <label>Nome:</label>
+      <input type="text" id="nome" required ></input><br></br>
+      <label>Assunto:</label>
+      <input type="text" id="assunto" required></input><br></br>
+      <label>Mensagem:</label>
+      <input type="textarea" id="mensagem" required></input><br></br>
+      <button type='submit'>Submit</button>
+      </form>
+      
     </div>
+
+    
   );
 };
 
