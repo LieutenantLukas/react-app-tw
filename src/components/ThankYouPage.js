3cbee9dcbe0b2e0ff8d4bdeb13cedcd01d8ef1c8
António <antonio.lourinho.teixeira@gmail.com>
2024-12-10 15:10:01 +0000

    Try

---
+++ src/components/ThankYouPage.js
@@ -0,0 +1,12 @@
+import React from 'react';
+
+const ThankYouPage = () => {
+  return (
+    <div className="thank-you">
+      <h1>Obrigado pela sua compra!</h1>
+      <p>Esperamos vê-lo novamente em breve!</p>
+    </div>
+  );
+};
+
+export default ThankYouPage;
