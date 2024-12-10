ece129a3a67b0e972de2dee7a45f6f82c89bd141
António <antonio.lourinho.teixeira@gmail.com>
2024-12-10 15:26:52 +0000

    ThankYouPage

--- src/components/ThankYouPage.js
+++ src/components/ThankYouPage.js
@@ -1,10 +1,15 @@
 import React from 'react';
+import { Link } from 'react-router-dom';
+import '../styles/ThankYouPage.css';
 
 const ThankYouPage = () => {
   return (
     <div className="thank-you">
       <h1>Obrigado pela sua compra!</h1>
-      <p>Esperamos vê-lo novamente em breve!</p>
+      <p>Esperamos vê-lo novamente em breve! Se tiver alguma dúvida, não hesite em contactar-nos.</p>
+      <Link to="/pokemons" className="back-to-home">
+        Voltar para o início
+      </Link>
     </div>
   );
 };
